7b6310da543f58b81343c9c1a6df276d5a8ff59c
Andres Acevedo - mran3 <x88andres@hotmail.com>
2020-07-31 03:01:37 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props){
+  const initialSearch = props.match?.query || ''
+  console.log(initialSearch)
+  const [query, setQuery] = React.useState(initialSearch);
+
+  const DATASET = ['Aayla Secura', 'Adi Gallia', 'Admiral Dodd Rancit', 'Admiral Firmus Piett', 'Admiral Gial Ackbar', 'Admiral Ozzel' ]; 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <SearchNames setQuery = {setQuery} query={query}/>
+      <SimpleList names = {DATASET} query={query} />
+    </>
+  )
+
+
+}
+
+function SearchNames(props) {
+
+  function handleChange(e) {
+    props.setQuery(e.target.value)
+  }
+  
+  function handleSubmit(e) {
+    console.log('update URL')
+    e.preventDefault()
+  }
+
+  const placeholder = props.query ? props.query : 'Enter your query.'
+
+  return (
+    <form onSubmit = {handleSubmit}>
+      <input type="text"
+        placeholder={placeholder}
+        onKeyUp={handleChange}/>
+    </form>
+  )
+}
+
+function SimpleList(props) {
+  const names = props.names;
+  const upperCaseQuery = props.query.toUpperCase()
+  const listItems = names.map((name, id) => {
+    const condition = name.toUpperCase().includes(upperCaseQuery) || upperCaseQuery === ''
+    
+    if(condition) {
+      return <li key={id}>{name}</li>    
+    }
+
+  }
+                             );
+
+  return (<ul>{listItems}</ul>)
 }
 
 export default App;
